9f52bf518273565028e63ce47d0a25f846f6de9a
Maggie <36987931+H-Mags@users.noreply.github.com>
2019-03-19 09:35:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ class App extends Component {
     return (
       <Router>
       <div className="App">
+      <script type="text/javascript">
+(function(d, src, c) { var t=d.scripts[d.scripts.length - 1],s=d.createElement('script');s.id='la_x2s6df8d';s.async=true;s.src=src;s.onload=s.onreadystatechange=function(){var rs=this.readyState;if(rs&&(rs!='complete')&&(rs!='loaded')){return;}c(this);};t.parentElement.insertBefore(s,t.nextSibling);})(document,
+'https://maggiela.ladesk.com/scripts/track.js',
+function(e){ LiveAgent.createButton('699a6813', e); });
+</script>
+    
 
         <Header />
 
